d329213f699c364c7c691adf5cdbc35fcf3eee2e
EsercitoHaki <esercitohaki2003@gmail.com>
2024-09-24 01:19:06 +0700

    Update State

---
+++ Behavioral/State/Code/src/main/java/org/example/OffState.java
@@ -0,0 +1,22 @@
+package org.example;
+
+public class OffState extends State{
+    public OffState(Phone phone)
+    {
+        super(phone);
+    }
+
+    @Override
+    public String onHome() {
+        phone.setState(new LockedState(phone));
+        return phone.home();
+    }
+
+    @Override
+    public String onOffOn() {
+        phone.setState(new LockedState(phone));
+        return phone.turnOn();
+    }
+
+
+}
